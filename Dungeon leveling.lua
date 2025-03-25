5321c87855dbeab0b96e39ac18d566ccacfad520
AlienMan100005 <Alien100005@outlook.com>
2025-03-25 15:44:59 -0400

    Update Dungeon leveling.lua

--- Dungeon leveling.lua
+++ Dungeon leveling.lua
@@ -514,7 +514,7 @@ local function startSecondScript()
     local blockingFrame = player.PlayerGui.ScreenInfo.Blocking
 
     local RunService = game:GetService("RunService")
-    local BLOCK_RANGE = 5
+    local BLOCK_RANGE = 10
     local enemiesInRange = {}
 
     local function invokeBlocking()
